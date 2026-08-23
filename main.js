75c3ac26ae28c619a89fba89729873e29913355d
Muftatihatul Ulya <ulya0136@gmail.com>
2026-08-23 17:28:36 +0700

    Correct alarm sound loop assignment
    Fixed the alarm sound loop assignment in the startTime function.

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+// const display = document.getElementById("display")
+// const start = document.getElementById("startbtn")
+// const pause = document.getElementById("pausebtn")
+// const reset = document.getElementById("resetbtn")
+
+// let timeleft = 5;
+// let interval = null; 
+
+// const updateTimer = () => {
+//     const minutes = Math.floor(timeleft / 60);
+//     const seconds = timeleft % 60;
+
+//     display.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+// }
+
+// const startTime = () => { 
+//     if (interval !== null) return;
+
+//     interval = setInterval(() => {
+//         timeleft--;
+//         updateTimer();
+        
+//         if (timeleft <= 0) { 
+//             clearInterval(interval);
+//             interval = null;
+//             const alarmSound = new Audio ("audio.mp3");
+//             alarmSound loop = true; 
+//             alarmSound.play().catch(error => {
+//                 console.log("alarm gagal diputar", error);
+//             });
+//             alert("Dah mateng! Telur rebusmu dah siap 🥚✨");
+//             alarmSound.pause();
+//             timeleft = 5; 
+//             updateTimer();
+//         }
+//     }, 1000); 
+// }
+
+// const resetTime = () => {
+//     clearInterval(interval);
+//     interval = null; 
+//     timeleft = 5;
+//     updateTimer();
+// }
+
+// const pauseTime = () => {
+//     clearInterval(interval);
+//     interval = null; 
+// }; 
+
+// start.addEventListener("click", startTime);
+// pause.addEventListener("click", pauseTime);
+// reset.addEventListener("click", resetTime);
+
+// updateTimer();
+
 const display = document.getElementById("display")
 const start = document.getElementById("startbtn")
 const pause = document.getElementById("pausebtn")
@@ -23,13 +79,17 @@ const startTime = () => {
         if (timeleft <= 0) { 
             clearInterval(interval);
             interval = null;
-            const alarmSound = new Audio ("audio.mp3");
-            alarmSound loop = true; 
+            
+            const alarmSound = new Audio("audio.mp3");
+            alarmSound.loop = true; // <--- PERBAIKAN DI SINI (pakai titik)
+            
             alarmSound.play().catch(error => {
                 console.log("alarm gagal diputar", error);
             });
+            
             alert("Dah mateng! Telur rebusmu dah siap 🥚✨");
             alarmSound.pause();
+            
             timeleft = 5; 
             updateTimer();
         }
